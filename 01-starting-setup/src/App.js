bf3d17eed4fc3b7e753f906bbc8c4d6fcf204e1d
Ivaylo Gabrovsky <ivaylo.gabrovsky@telus.com>
2021-12-15 15:17:09 -0500

    add ExpenseItem

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,7 +1,14 @@
+import React from "react";
+
+import ExpenseItem from './components/ExpenseItem'
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem date="March 28th 2021" title="Car Insurance" price="$294.67"></ExpenseItem>
+      <ExpenseItem date="Apr 28th 2021" title="Home Insurance" price="$100.67"></ExpenseItem>
+      <ExpenseItem date="Jan 28th 2021" title="Car Insurance" price="$344.67"></ExpenseItem>
     </div>
   );
 }
